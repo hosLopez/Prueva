29d75dffe2b767fa5006f21043826b3f71172448
Hos <hos.fl700@gmail,com>
2023-04-21 18:27:39 -0400

    Se puso un ford

--- src/main/java/com/schumager/pruevagit/PruevaGit.java
+++ src/main/java/com/schumager/pruevagit/PruevaGit.java
@@ -13,5 +13,9 @@ public class PruevaGit {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World2!");
+        System.err.println("Hello Wordd3");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("com.schumager.pruevagit.PruevaGit.main() "+i);
+        }
     }
 }
