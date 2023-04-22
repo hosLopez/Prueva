50e9687c488a7a530747527d968269f946f2c2ca
Schumager <hos.fl700@gmail.com>
2023-04-22 10:47:06 -0400

    se agrego dos Clases

---
+++ src/main/java/com/schumager/controller/PersonaController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.schumager.controller;
+
+import com.schumager.model.Persona;
+import java.util.List;
+
+/**
+ *
+ * @author Schumager
+ */
+public class PersonaController {
+    List<Persona> lista;   
+    public PersonaController(){
+        
+    }
+    
+}
